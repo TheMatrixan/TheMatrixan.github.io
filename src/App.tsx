b9a41b3f8e7cc2caf1f98b5ce11e0e3375869d5c
Mateusz Lesiak <mateusz.lesiak@wp.pl>
2019-11-26 13:37:20 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mateusz Lesiak</h1>
+        <p>Strona w budowie</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
