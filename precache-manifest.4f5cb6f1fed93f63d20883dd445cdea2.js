4b2a3f817551b48f1a33dc405dc9c70d825fac7c
Mateusz Lesiak <210248@edu.p.lodz.pl>
2019-12-28 15:22:14 +0100

    Updates

---
+++ precache-manifest.4f5cb6f1fed93f63d20883dd445cdea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8174fccdc8956b9f94ef66417d63c63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f309d847d2041a069a4f",
+    "url": "/static/css/main.f2b55a92.chunk.css"
+  },
+  {
+    "revision": "e09ca3ee9653febd8f3c",
+    "url": "/static/js/2.a46abbd4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a46abbd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f309d847d2041a069a4f",
+    "url": "/static/js/main.774d1122.chunk.js"
+  },
+  {
+    "revision": "7b8163dd7bc4614b269a",
+    "url": "/static/js/runtime-main.66c712a2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
